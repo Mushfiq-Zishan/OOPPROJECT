a18f554b54ac71b054d72d4ed68a4dfbbb0c42db
Mushfiq-Zishan <mushfiqzishan545@gmail.com>
2026-04-18 00:39:03 +0600

    Update RailwayStaff.java

--- RailwayStaff.java
+++ RailwayStaff.java
@@ -1,11 +1,16 @@
-// 4. ================= STAFF CLASS =================
 class RailwayStaff extends Person {
-    int staffId;
+    private int staffId;
+
     public RailwayStaff(String name, int id) {
-        super(name, null, null); // Staff er email/phone dorkar nai ekhane
+        super(name, null, null);
         this.staffId = id;
     }
+
+    public void updateSchedule() { System.out.println("Schedule updated."); }
+    public void modifyTicket() { System.out.println("Ticket modified."); }
+    public void addOffer() { System.out.println("Offer added."); }
+
     void verify() {
-        System.out.println("Verification: Done by Staff " + name + " (ID: " + staffId + ")");
+        System.out.println("Verification: Done by Staff " + getName() + " (ID: " + staffId + ")");
     }
 }
