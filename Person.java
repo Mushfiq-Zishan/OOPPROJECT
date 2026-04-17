df2bda085d70b5cd337d01c4b7fc498e70f40b1a
Mushfiq-Zishan <mushfiqzishan545@gmail.com>
2026-04-18 00:37:52 +0600

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,10 +1,18 @@
-// 2. ================= PERSON CLASS =================
-class Person {
-    String name, email, phone;
+abstract class Person {
+    private String name;
+    private String email;
+    private String phoneNumber;
+
     public Person(String name, String email, String phone) {
         this.name = name;
         this.email = email;
-        this.phone = phone;
+        this.phoneNumber = phone;
     }
+
     public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPhoneNumber() { return phoneNumber; }
+
+    public void login() { System.out.println(name + " logged in."); }
+    public void logout() { System.out.println(name + " logged out."); }
 }
